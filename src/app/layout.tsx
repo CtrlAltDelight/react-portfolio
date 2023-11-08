1409444681e68c067424844c5cbccde2b1853fab
CtrlAltDelight <lbchigges@gmail.com>
2023-11-07 21:57:31 -0500

    Changed title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Luke Chigges Freelancer',
+  title: 'Chigges Designs',
   description: 'Hire a full stack developer and designer.',
 }
 
