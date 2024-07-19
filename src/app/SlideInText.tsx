f8100a2a70719bc6e9b4b6aad1a0d9506b4782b9
CtrlAltDelight <lbchigges@gmail.com>
2024-07-18 21:08:07 -0400

    Changed site to be more of an overview of my resume.

--- src/app/SlideInText.tsx
+++ src/app/SlideInText.tsx
@@ -1,4 +1,4 @@
-import { useInView, animated } from '@react-spring/web';
+import { useInView, animated } from "@react-spring/web";
 
 interface Props {
 	className?: string;
@@ -7,25 +7,28 @@ interface Props {
 	to?: number;
 }
 
-function SlideInText({ className, children, from=-100, to=0 }: Props) {
-
+function SlideInText({ className, children, from = 100, to = 0 }: Props) {
 	const [ref, springs] = useInView(
-			() => ({
-					   from: {
-						   opacity: 0,
-						   x: from,
-					   },
-					   to: {
-						   opacity: 1,
-						   x: to,
-					   },
-				   }),
-				   {
-					   rootMargin: '-15% 0%',
-				   }
-			);
+		() => ({
+			from: {
+				opacity: 0,
+				y: from,
+			},
+			to: {
+				opacity: 1,
+				y: to,
+			},
+		}),
+		{
+			rootMargin: "-30% 0%",
+		},
+	);
 
-	return <animated.div ref={ref} style={springs} className={className}>{children}</animated.div>;
+	return (
+		<animated.div ref={ref} style={springs} className={className}>
+			{children}
+		</animated.div>
+	);
 }
 
 export default SlideInText;
